3cedb3d367a5790137555a61b68890fce818b893
SAKTHI M <msakthi150@gmail.com>
2023-03-01 07:49:08 +0530

    c

--- 19.cpp
+++ 19.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+#include <fstream>
+using std::ofstream; 
 using namespace std;
+ 
 int main()
 {
-    
+ofstream fout;
+fout.open("sort.txt");
+int n,i,j,t;
+cin>>n;
+int a[n];
+for(i=0;i<n;i++)
+{
+    cin>>a[i];
+}
+for(i=0;i<n;i++)
+{
+    fout<<a[i]<<" ";
+}
+fout.close();
+ifstream file;
+file.open("sort.txt");
+int num;
+int b[n];
+i=0;
+while(file>>num)
+{
+cout<<endl;
+    a[i]=num;
+    cout<<num<<endl;
+    i++;
+}
+for(int i=0;i<n;i++)
+{
+    for(int j=i+1;j<n;j++)
+    {
+        if(a[i]>a[j])
+        {
+          t=a[i];
+          a[i]=a[j];
+          a[j]=t;
+        }
+    }
+}
+for(int i=0;i<n;i++)
+{
+    cout<<a[i]<<endl;
+}
+file.close();
     return 0;
 }
